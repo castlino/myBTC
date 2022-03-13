c8ce18fae1c997b326db6720ab7cb1051321f1c9
LiNo <felino@mybos.com>
2022-03-13 18:26:22 +1100

    Configured laravel/passport. (see https://laravel.com/docs/8.x/passport#main-content or https://blog.logrocket.com/laravel-passport-a-tutorial-and-example-build/) Edited laravel/passport to use Forked version at laravel/passport-mybos (dev-10.x.mybos).    - Github url: https://github.com/linoMybos/passport Update User model to use the jessengers/mongodb Authenticalbe class. (the forked laravel/passport does the same to all User model references)

--- api-backend/app/Models/User.php
+++ api-backend/app/Models/User.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+//use Illuminate\Foundation\Auth\User as Authenticatable;
+use Jenssegers\Mongodb\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;  //add the namespace
 
 class User extends Authenticatable
 {
